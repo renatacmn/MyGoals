5d9ce63a49dcc57ba689dd0b1e89faf51e8c945f
Renata Cristina <cmn.renata@gmail.com>
2019-09-16 00:08:44 +0200

    Implementing repository

--- app/src/main/java/br/com/mygoals/base/api/MyGoalsRepository.kt
+++ app/src/main/java/br/com/mygoals/base/api/MyGoalsRepository.kt
@@ -1,4 +1,25 @@
 package br.com.mygoals.base.api
 
-class MyGoalsRepository {
+import br.com.mygoals.base.api.models.Feed
+import br.com.mygoals.base.api.models.SavingsGoals
+import br.com.mygoals.base.api.models.SavingsRules
+import io.reactivex.Single
+import javax.inject.Inject
+
+class MyGoalsRepository @Inject constructor(
+    private val api: MyGoalsApi
+) {
+
+    fun getSavingsGoals(): Single<SavingsGoals> {
+        return api.getSavingsGoals()
+    }
+
+    fun getSavingsRules(): Single<SavingsRules> {
+        return api.getSavingsRules()
+    }
+
+    fun getFeed(id: Int): Single<Feed> {
+        return api.getFeed(id)
+    }
+
 }
