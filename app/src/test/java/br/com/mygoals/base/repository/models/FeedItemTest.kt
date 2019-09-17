0c28c38571d8732529ef4f7698d1bb84f5775e3f
Renata Cristina <cmn.renata@gmail.com>
2019-09-17 22:51:01 +0200

    Test FeedItem and Goal's methods

--- app/src/test/java/br/com/mygoals/base/repository/models/FeedItemTest.kt
+++ app/src/test/java/br/com/mygoals/base/repository/models/FeedItemTest.kt
@@ -1,5 +1,19 @@
 package br.com.mygoals.base.repository.models
 
-import org.junit.Assert.*
+import br.com.mygoals.testUtil.FeedTestDataUtil
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
 
-class FeedItemTest
+class FeedItemTest {
+
+    private val testDataUtil = FeedTestDataUtil()
+
+    @Test
+    fun getRule_shouldReturnRuleWithIdAndNoType() {
+        val id = 1
+        val feedItem = testDataUtil.feedItem(id)
+        val expected = Rule(id, null)
+        assertEquals(expected, feedItem?.getRule())
+    }
+
+}
--- app/src/test/java/br/com/mygoals/base/repository/models/GoalTest.kt
+++ app/src/test/java/br/com/mygoals/base/repository/models/GoalTest.kt
@@ -1,5 +1,64 @@
 package br.com.mygoals.base.repository.models
 
-import org.junit.Assert.*
+import android.content.Context
+import br.com.mygoals.R
+import br.com.mygoals.testUtil.BaseUnitTest
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.anyInt
+import org.mockito.Mockito.mock
 
-class GoalTest
+class GoalTest: BaseUnitTest() {
+
+    private lateinit var context: Context
+
+    @Before
+    fun setUp() {
+        context = mock(Context::class.java)
+    }
+
+    @Test
+    fun getValueText_ifTargetAmountIsNull_shouldReturnCorrectString() {
+        val currentBalance = 0f
+        val goal = Goal(
+            1,
+            "Goal 1",
+            "",
+            currentBalance,
+            null
+        )
+
+        whenever(context.getString(anyInt(), any())).thenReturn("")
+
+        goal.getValueText(context)
+        verify(context).getString(
+            R.string.list_item_goal_value_without_target,
+            currentBalance
+        )
+    }
+
+    @Test
+    fun getValueText_ifTargetAmountIsNotNull_shouldReturnCorrectString() {
+        val currentBalance = 0f
+        val targetAmount = 0f
+        val goal = Goal(
+            1,
+            "Goal 1",
+            "",
+            currentBalance,
+            targetAmount
+        )
+
+        whenever(context.getString(anyInt(), any())).thenReturn("")
+
+        goal.getValueText(context)
+        verify(context).getString(
+            R.string.list_item_goal_value_with_target,
+            currentBalance,
+            targetAmount
+        )
+    }
+
+}
